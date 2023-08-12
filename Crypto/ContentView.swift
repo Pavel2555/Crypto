293dabc9300cf9650b09783921813e0143559d9c
Pavel Beskorovainyi <beskor1993@icloud.com>
2023-08-12 19:43:49 +0200

    add assets + set Theme

--- Crypto/ContentView.swift
+++ Crypto/ContentView.swift
@@ -8,19 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    ZStack {
+      Color.theme.background
+        .ignoresSafeArea()
+      
+      VStack(spacing: 40) {
+        Text("Accent color")
+          .foregroundColor(.theme.accent)
+        
+        Text("Secondary Text Color")
+          .foregroundColor(.theme.secondaryText)
+        
+        Text("Red Color")
+          .foregroundColor(.theme.red)
+        
+        Text("Green Color")
+          .foregroundColor(.theme.green)
+      }
+      .font(.headline)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- Crypto/CryptoApp.swift
+++ Crypto/CryptoApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct CryptoApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
