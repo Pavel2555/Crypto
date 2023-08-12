814763ada5a67761a2c56f5408f1882ba2ec145d
Pavel2555 <86742948+Pavel2555@users.noreply.github.com>
2023-08-12 19:15:06 +0200

    Add files via upload

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Павел Бескоровайный on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
