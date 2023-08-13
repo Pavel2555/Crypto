9adf934902d3945ff57945dd0f0574bc0699b819
Pavel Beskorovainyi <beskor1993@icloud.com>
2023-08-13 12:19:05 +0200

    filtering with combine!

--- Crypto/Core/Home/ViewModels/HomeViewModel.swift
+++ Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -23,11 +23,36 @@ class HomeViewModel: ObservableObject {
   }
   
   func addSubscribers() {
+    
+    //update allCoins
     dataService.$allCoins
       .sink { [weak self] coins in
         guard let self = self else { return }
         self.allCoins = coins
       }
       .store(in: &cancellables)
+    
+    $searchText
+      .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+      .combineLatest(dataService.$allCoins)
+      .map(filterCoins)
+      .sink { [weak self] returnedCoins in
+        self?.allCoins = returnedCoins
+      }
+      .store(in: &cancellables)
+  }
+  
+  private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+    guard !text.isEmpty else {
+      return coins
+    }
+    
+    let lowercasedText = text.lowercased()
+    
+    return coins.filter({ coin in
+      return coin.name.contains(lowercasedText) ||
+      coin.symbol.contains(lowercasedText) ||
+      coin.id.contains(lowercasedText)
+    })
   }
 }
